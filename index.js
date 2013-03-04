de5d15aa3919494ad7f89f4498a9207de6b093a4
anders elo <anders@kaerus.com>
2013-03-04 20:48:06 +0100

    initial

---
+++ index.js
@@ -0,0 +1,52 @@
+var Promise = require('promise');
+
+var cached = {}; 
+ // Script ////////////////////////////////////////////////////////////////////////////
+
+function Script(file,timeout) {
+    var loaded = cached[file];
+
+    if(loaded) return loaded;
+
+    loaded = cached[file] = new Promise();
+
+    if(timeout) loaded.timeout(timeout);
+
+    var head = document.getElementsByTagName("head")[0];
+    var script = document.createElement("script");
+
+    /* add timestamp to bypass cache */
+    script.src = file+'?'+Date.now();
+    script.async = true;
+    script.defer = true;
+
+    function onloaded(event) {
+        loaded.attach(file).fulfill(event);
+    }
+
+    function onerror(event) {
+        loaded.attach(file).reject(event);
+    }
+
+    if(script.readyState) {
+        script.onreadystatechange = function(event) {
+            /* FIXME: IE on 404 error hell */
+            if(this.readyState === "loaded" || 
+                this.readyState === "complete") {
+                this.onreadystatechange = null;
+                onloaded(event);
+            } else {
+                onerror(event);
+            }   
+        }
+    } else {
+        script.onload = onloaded;
+        script.onerror = onerror;
+    }   
+
+    head.appendChild(script);
+
+    return loaded;
+}
+
+module.exports = Script;
